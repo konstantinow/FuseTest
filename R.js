8faa4642b4534a065184d5a1cbf2e687ed94d95b
applexis <alent.1301@gmail.com>
2017-11-10 13:30:53 +0300

    sha1, pages

---
+++ R.js
@@ -0,0 +1,10 @@
+var imageMainBG = "../Assets/Images/background.png"
+module.exports = {
+	imageMainBG: imageMainBG,
+	imageEye: "../Assets/Images/eye.png",
+	imageEyeShow: "../Assets/Images/eye_show.png",
+	imageFB: "../Assets/Images/facebook_logo.png",
+	imageLogo: "../Assets/Images/logo.png",
+	imageNoAvatar: "../Assets/Images/no_avatar_circle.png",
+	imageCircleAdd: "../Assets/Images/circle_add.png",
+}
